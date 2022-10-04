480ad82ce05802bc5373395b218137a46cc9b430
BautistaMendibe <bautistamendibe10@gmail.com>
2022-10-04 14:49:30 -0300

    Creé la clase Participante y configuré el tsconfig con "strictPropertyInitialization": false para que no me obligue a inicializar los atributos de la clase de una

--- src/app/cuanto-puso-component/cuanto-puso-component.component.ts
+++ src/app/cuanto-puso-component/cuanto-puso-component.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Participante } from "../models/participante";
 
 @Component({
   selector: 'app-cuanto-puso-component',
   templateUrl: './cuanto-puso-component.component.html',
   styleUrls: ['./cuanto-puso-component.component.css']
 })
-export class CuantoPusoComponentComponent implements OnInit {
+export class CuantoPusoComponentComponent{
 
-  constructor() { }
 
-  ngOnInit(): void {
-  }
 
 }
---
+++ src/app/models/participante.ts
@@ -0,0 +1,5 @@
+export class Participante {
+  id: number;
+  nombre: string;
+  cantidad: number;
+}
