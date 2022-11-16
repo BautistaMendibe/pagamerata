10fac54643c84562b1ae4068a7e4979a372a5c59
BautistaMendibe <bautistamendibe10@gmail.com>
2022-11-16 12:23:20 -0300

    Ahora cuando no pone nada en la cantidad lo toma como 0

--- src/app/cuanto-puso-component/cuanto-puso-component.component.ts
+++ src/app/cuanto-puso-component/cuanto-puso-component.component.ts
@@ -13,9 +13,15 @@ export class CuantoPusoComponentComponent{
   participanteSeleccionado:Participante = new Participante();
 
   agregarEditarParticipante(){
-    if(this.participanteSeleccionado.nombre == null || this.participanteSeleccionado.cantidad == null){
-      alert("Debe agregar todos los campos")
+    if(this.participanteSeleccionado.nombre == null){
+      alert("Debe agregarle un nombre")
     } else {
+
+      // Si la cantidad es nula poner como 0
+      if(this.participanteSeleccionado.cantidad == null){
+        this.participanteSeleccionado.cantidad = 0;
+      }
+
       // Si el id=0 no hay ningún empleado seleccionado y se quiere agregar uno
       if(this.participanteSeleccionado.id === 0){
         this.participanteSeleccionado.id = this.participantesArray.length + 1;
