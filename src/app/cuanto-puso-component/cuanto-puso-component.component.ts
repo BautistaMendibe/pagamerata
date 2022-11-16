c7616af56a39e1f4f39a3bd1c171eeca4463ada5
BautistaMendibe <bautistamendibe10@gmail.com>
2022-11-16 11:30:35 -0300

    Ahora verifica que los campos estén completos antes de agregar el participante

--- src/app/cuanto-puso-component/cuanto-puso-component.component.ts
+++ src/app/cuanto-puso-component/cuanto-puso-component.component.ts
@@ -13,19 +13,17 @@ export class CuantoPusoComponentComponent{
   participanteSeleccionado:Participante = new Participante();
 
   agregarEditarParticipante(){
+    if(this.participanteSeleccionado.nombre == null || this.participanteSeleccionado.cantidad == null){
+      alert("Debe agregar todos los campos")
+    } else {
+      // Si el id=0 no hay ningún empleado seleccionado y se quiere agregar uno
+      if(this.participanteSeleccionado.id === 0){
+        this.participanteSeleccionado.id = this.participantesArray.length + 1;
+        this.participantesArray.push(this.participanteSeleccionado);
+      }
 
-    // Ver qué onda esto por qué no funciona
-    //if(this.participanteSeleccionado.nombre === null || this.participanteSeleccionado.cantidad === null){
-    //  alert("Debe agregar todos los campos")
-    //}
-
-    // Si el id=0 no hay ningún empleado seleccionado y se quiere agregar uno
-    if(this.participanteSeleccionado.id === 0){
-      this.participanteSeleccionado.id = this.participantesArray.length + 1;
-      this.participantesArray.push(this.participanteSeleccionado);
+      this.participanteSeleccionado = new Participante();
     }
-
-    this.participanteSeleccionado = new Participante();
   }
 
   abrirParaEditar(participante: Participante){
