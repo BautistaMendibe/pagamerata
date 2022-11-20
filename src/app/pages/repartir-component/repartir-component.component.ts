c51033dc1397753566ba1b291451ecf7be14a4ce
BautistaMendibe <bautistamendibe10@gmail.com>
2022-11-20 11:48:06 -0300

    Se calcula la cantidad de participantes

--- src/app/pages/repartir-component/repartir-component.component.ts
+++ src/app/pages/repartir-component/repartir-component.component.ts
@@ -9,21 +9,33 @@ import {DataService} from "../../components/services/data.service";
 export class RepartirComponentComponent implements OnInit {
 
   public total: number;
+  private cantidadParticipantes: number;
+  private cantidadCadaUno: number;
 
   constructor(public dataService: DataService) { }
 
   ngOnInit(): void {
     this.total = this.calcularTotal();
+    this.cantidadCadaUno = this.calcularCantidadCadaUno(this.total, this.cantidadParticipantes);
+    console.log(this.cantidadParticipantes);
   }
 
   calcularTotal(): number{
     let subTotal = 0;
+    let cantidadParticipantes = 0;
 
     this.dataService.participantes.forEach( function (p) {
       subTotal += p.cantidad;
+      cantidadParticipantes ++;
     });
 
+    this.cantidadParticipantes = cantidadParticipantes;
+
     return subTotal;
+  }
+
+  calcularCantidadCadaUno(total: number, cantidadParticipantes: number): number{
+    return 1;
   }
 
 }
